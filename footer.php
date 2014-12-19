3c25b0dde22b8c6ab6247765698770f09d168980
Aleksejs Popovs <aleksejs@popovs.lv>
2014-12-19 20:58:34 +0200

    remove branding

--- footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-	echo "<span class='footer'><a href='http://github.com/popoffka/dungeons'>Dungeons</a> by mariofag. Rev. ";
-	$rev = system("git rev-parse HEAD");
-	echo "<br />\n";
-	if ($_SERVER["HTTP_HOST"] == "dun.shell.tor.hu") 
-		echo 'Host kindly provided by <a href="http://shell.tor.hu">shell.tor.hu</a>, great web host with shell access, real hardware and low prices. More info at <a href="http://cp.tor.hu">cp.tor.hu</a>.</span>';
-?>
